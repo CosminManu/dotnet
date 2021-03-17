bb4eca6f9e1ebcbe7befcd40f7446ae8e7594ae0
CosminManu <75031246+CosminManu@users.noreply.github.com>
2021-03-18 00:06:27 +0200

    Add files via upload

---
+++ codes/GradeBook/src/gradebook/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace gradebook
+{
+    class Book
+    {        
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Book book = new Book();
+            var x = 34.2;
+            var y = 1.1;
+            var z = x + y;
+            Console.WriteLine(z);
+
+            // var numbers = new[] {31, 33.9, 34.5, 12};
+            // for(int i =0; i<numbers.Length; i++)
+            // {
+            //     s = s + numbers[i];
+            // }
+            // Console.WriteLine(s);
+
+            var s = 0.0;
+            var grades = new List<double>() {8, 8, 9.5, 9};
+            grades.Add(55.7);
+            foreach(var number in grades)
+            {
+                s += number;
+            }
+            s = s /grades.Count;
+            Console.WriteLine($"The average grade is {s:N1}");
+
+
+            // if(args.Length > 0)
+            // {
+            //     Console.WriteLine($"Hello, {args[0]}!");
+            // }
+            // else
+            // {
+            //     Console.WriteLine("Hello!");
+            // }
+        }
+    }
+}
