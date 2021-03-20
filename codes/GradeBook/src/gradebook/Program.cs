5796801dd1156c1540293a98733e650662c92265
CosminManu <75031246+CosminManu@users.noreply.github.com>
2021-03-21 00:25:48 +0200

    updated Program.cs in psvm

--- codes/GradeBook/src/gradebook/Program.cs
+++ codes/GradeBook/src/gradebook/Program.cs
@@ -1,49 +1,25 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 namespace gradebook
 {
-    class Book
-    {        
-
-    }
     class Program
     {
         static void Main(string[] args)
         {
+            // se apeleaza la infinit
+            // var p = new Program();
+            // Program.Main(args);
 
-            Book book = new Book();
-            var x = 34.2;
-            var y = 1.1;
-            var z = x + y;
-            Console.WriteLine(z);
-
-            // var numbers = new[] {31, 33.9, 34.5, 12};
-            // for(int i =0; i<numbers.Length; i++)
-            // {
-            //     s = s + numbers[i];
-            // }
-            // Console.WriteLine(s);
-
-            var s = 0.0;
-            var grades = new List<double>() {8, 8, 9.5, 9};
-            grades.Add(55.7);
-            foreach(var number in grades)
-            {
-                s += number;
-            }
-            s = s /grades.Count;
-            Console.WriteLine($"The average grade is {s:N1}");
-
-
-            // if(args.Length > 0)
-            // {
-            //     Console.WriteLine($"Hello, {args[0]}!");
-            // }
-            // else
-            // {
-            //     Console.WriteLine("Hello!");
-            // }
+            var book = new Book("Cartea Junglei");      // invoking consturctor
+            book.AddBook(12.2);
+            book.AddBook(99.9);
+            //book.grades.Add(123);     // private modifier
+            book.AddBook(123);
+            book.PrintBook();
+            book.Statistics();
+           
         }
     }
 }
+
