8f96622e0b7d334bcf704aa34f762bbcc87ac924
CosminManu <75031246+CosminManu@users.noreply.github.com>
2021-04-04 00:28:09 +0300

    order of operation, type of op

---
+++ Basic Calculator/Basic Calculator/Operation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Calculator
+{
+    public class Operation
+    {
+        public string LeftSide { get; set; }
+        public string RightSide { get; set; }
+
+        public OperationType OperationType { get; set; }
+        public OperationType InnerOperation { get; set; }       // de vazut
+
+        public Operation()
+        {
+            this.LeftSide = "";
+            this.RightSide = "";
+        }
+
+    }
+}
---
+++ Basic Calculator/Basic Calculator/OperationType.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Calculator
+{
+    public enum OperationType   // fol ca un tip de data
+    {
+        /// <summary>
+        /// Adds two numbers
+        /// </summary>
+        Add,
+
+        /// <summary>
+        /// Substracts a number from another
+        /// </summary>
+        Minus,
+
+        /// <summary>
+        /// Divides two numbers
+        /// </summary>
+        Divide,
+
+        /// <summary>
+        /// Multiplies two given numbers
+        /// </summary>
+        Multiply,
+    }
+}
