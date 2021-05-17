05a585b99ad22772319f99ef374a73684e6ce779
CosminManu <75031246+CosminManu@users.noreply.github.com>
2021-05-17 05:13:55 +0300

    Add files via upload

---
+++ Fast Food demo/EditForm.Designer.cs
@@ -0,0 +1,341 @@
+﻿
+namespace Fast_Food_demo
+{
+    partial class EditForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(EditForm));
+            this.panelTop = new System.Windows.Forms.Panel();
+            this.MaximizeBtn = new System.Windows.Forms.Button();
+            this.CloseBtn = new System.Windows.Forms.Button();
+            this.MinimizeBtn = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.grBUserDetails = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbUsername = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBoxVarsta = new System.Windows.Forms.ComboBox();
+            this.labelVars = new System.Windows.Forms.Label();
+            this.tbEmail = new System.Windows.Forms.TextBox();
+            this.labelEmail = new System.Windows.Forms.Label();
+            this.tbNume = new System.Windows.Forms.TextBox();
+            this.labelVarsta = new System.Windows.Forms.Label();
+            this.tbPrenume = new System.Windows.Forms.TextBox();
+            this.labelDenumire = new System.Windows.Forms.Label();
+            this.btnSalveaza = new System.Windows.Forms.Button();
+            this.btnRenunta = new System.Windows.Forms.Button();
+            this.panelTop.SuspendLayout();
+            this.grBUserDetails.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelTop
+            // 
+            this.panelTop.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(178)))), ((int)(((byte)(8)))), ((int)(((byte)(55)))));
+            this.panelTop.Controls.Add(this.button2);
+            this.panelTop.Controls.Add(this.button1);
+            this.panelTop.Controls.Add(this.MaximizeBtn);
+            this.panelTop.Controls.Add(this.CloseBtn);
+            this.panelTop.Controls.Add(this.MinimizeBtn);
+            this.panelTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTop.Location = new System.Drawing.Point(0, 0);
+            this.panelTop.Name = "panelTop";
+            this.panelTop.Size = new System.Drawing.Size(382, 20);
+            this.panelTop.TabIndex = 2;
+            // 
+            // MaximizeBtn
+            // 
+            this.MaximizeBtn.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.MaximizeBtn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("MaximizeBtn.BackgroundImage")));
+            this.MaximizeBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.MaximizeBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.MaximizeBtn.Location = new System.Drawing.Point(748, 1);
+            this.MaximizeBtn.Name = "MaximizeBtn";
+            this.MaximizeBtn.Size = new System.Drawing.Size(21, 20);
+            this.MaximizeBtn.TabIndex = 17;
+            this.MaximizeBtn.UseVisualStyleBackColor = false;
+            // 
+            // CloseBtn
+            // 
+            this.CloseBtn.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.CloseBtn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("CloseBtn.BackgroundImage")));
+            this.CloseBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.CloseBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.CloseBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CloseBtn.Location = new System.Drawing.Point(779, 1);
+            this.CloseBtn.Name = "CloseBtn";
+            this.CloseBtn.Size = new System.Drawing.Size(21, 21);
+            this.CloseBtn.TabIndex = 17;
+            this.CloseBtn.UseVisualStyleBackColor = false;
+            // 
+            // MinimizeBtn
+            // 
+            this.MinimizeBtn.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.MinimizeBtn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("MinimizeBtn.BackgroundImage")));
+            this.MinimizeBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.MinimizeBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.MinimizeBtn.FlatAppearance.BorderSize = 0;
+            this.MinimizeBtn.Location = new System.Drawing.Point(716, 1);
+            this.MinimizeBtn.Name = "MinimizeBtn";
+            this.MinimizeBtn.Size = new System.Drawing.Size(21, 21);
+            this.MinimizeBtn.TabIndex = 1;
+            this.MinimizeBtn.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(337, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(21, 21);
+            this.button1.TabIndex = 18;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.Location = new System.Drawing.Point(300, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(21, 21);
+            this.button2.TabIndex = 3;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // grBUserDetails
+            // 
+            this.grBUserDetails.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.grBUserDetails.AutoSize = true;
+            this.grBUserDetails.Controls.Add(this.label3);
+            this.grBUserDetails.Controls.Add(this.tbPassword);
+            this.grBUserDetails.Controls.Add(this.label1);
+            this.grBUserDetails.Controls.Add(this.tbUsername);
+            this.grBUserDetails.Controls.Add(this.label2);
+            this.grBUserDetails.Controls.Add(this.comboBoxVarsta);
+            this.grBUserDetails.Controls.Add(this.labelVars);
+            this.grBUserDetails.Controls.Add(this.tbEmail);
+            this.grBUserDetails.Controls.Add(this.labelEmail);
+            this.grBUserDetails.Controls.Add(this.tbNume);
+            this.grBUserDetails.Controls.Add(this.labelVarsta);
+            this.grBUserDetails.Controls.Add(this.tbPrenume);
+            this.grBUserDetails.Controls.Add(this.labelDenumire);
+            this.grBUserDetails.Location = new System.Drawing.Point(39, 52);
+            this.grBUserDetails.Name = "grBUserDetails";
+            this.grBUserDetails.Size = new System.Drawing.Size(300, 298);
+            this.grBUserDetails.TabIndex = 3;
+            this.grBUserDetails.TabStop = false;
+            this.grBUserDetails.Text = "User details";
+            // 
+            // label3
+            // 
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Location = new System.Drawing.Point(6, 190);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(284, 1);
+            this.label3.TabIndex = 7;
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.Location = new System.Drawing.Point(83, 244);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.Size = new System.Drawing.Size(195, 20);
+            this.tbPassword.TabIndex = 13;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 247);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Password";
+            // 
+            // tbUsername
+            // 
+            this.tbUsername.Location = new System.Drawing.Point(83, 205);
+            this.tbUsername.Name = "tbUsername";
+            this.tbUsername.Size = new System.Drawing.Size(195, 20);
+            this.tbUsername.TabIndex = 11;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 208);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Username";
+            // 
+            // comboBoxVarsta
+            // 
+            this.comboBoxVarsta.FormattingEnabled = true;
+            this.comboBoxVarsta.Items.AddRange(new object[] {
+            " < 16 years",
+            "16 - 21 years",
+            " > 21 years"});
+            this.comboBoxVarsta.Location = new System.Drawing.Point(83, 143);
+            this.comboBoxVarsta.Name = "comboBoxVarsta";
+            this.comboBoxVarsta.Size = new System.Drawing.Size(195, 21);
+            this.comboBoxVarsta.TabIndex = 9;
+            // 
+            // labelVars
+            // 
+            this.labelVars.AutoSize = true;
+            this.labelVars.Location = new System.Drawing.Point(17, 146);
+            this.labelVars.Name = "labelVars";
+            this.labelVars.Size = new System.Drawing.Size(26, 13);
+            this.labelVars.TabIndex = 8;
+            this.labelVars.Text = "Age";
+            // 
+            // tbEmail
+            // 
+            this.tbEmail.Location = new System.Drawing.Point(83, 108);
+            this.tbEmail.Name = "tbEmail";
+            this.tbEmail.Size = new System.Drawing.Size(195, 20);
+            this.tbEmail.TabIndex = 5;
+            // 
+            // labelEmail
+            // 
+            this.labelEmail.AutoSize = true;
+            this.labelEmail.Location = new System.Drawing.Point(17, 111);
+            this.labelEmail.Name = "labelEmail";
+            this.labelEmail.Size = new System.Drawing.Size(32, 13);
+            this.labelEmail.TabIndex = 4;
+            this.labelEmail.Text = "Email";
+            // 
+            // tbNume
+            // 
+            this.tbNume.Location = new System.Drawing.Point(83, 64);
+            this.tbNume.Name = "tbNume";
+            this.tbNume.Size = new System.Drawing.Size(195, 20);
+            this.tbNume.TabIndex = 3;
+            // 
+            // labelVarsta
+            // 
+            this.labelVarsta.AutoSize = true;
+            this.labelVarsta.Location = new System.Drawing.Point(14, 67);
+            this.labelVarsta.Name = "labelVarsta";
+            this.labelVarsta.Size = new System.Drawing.Size(58, 13);
+            this.labelVarsta.TabIndex = 2;
+            this.labelVarsta.Text = "Last Name";
+            // 
+            // tbPrenume
+            // 
+            this.tbPrenume.Location = new System.Drawing.Point(83, 24);
+            this.tbPrenume.Name = "tbPrenume";
+            this.tbPrenume.Size = new System.Drawing.Size(195, 20);
+            this.tbPrenume.TabIndex = 1;
+            // 
+            // labelDenumire
+            // 
+            this.labelDenumire.AutoSize = true;
+            this.labelDenumire.Location = new System.Drawing.Point(14, 27);
+            this.labelDenumire.Name = "labelDenumire";
+            this.labelDenumire.Size = new System.Drawing.Size(57, 13);
+            this.labelDenumire.TabIndex = 0;
+            this.labelDenumire.Text = "First Name";
+            // 
+            // btnSalveaza
+            // 
+            this.btnSalveaza.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnSalveaza.Location = new System.Drawing.Point(39, 376);
+            this.btnSalveaza.Name = "btnSalveaza";
+            this.btnSalveaza.Size = new System.Drawing.Size(108, 44);
+            this.btnSalveaza.TabIndex = 5;
+            this.btnSalveaza.Text = "Save";
+            this.btnSalveaza.UseVisualStyleBackColor = true;
+            // 
+            // btnRenunta
+            // 
+            this.btnRenunta.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnRenunta.Location = new System.Drawing.Point(231, 376);
+            this.btnRenunta.Name = "btnRenunta";
+            this.btnRenunta.Size = new System.Drawing.Size(108, 44);
+            this.btnRenunta.TabIndex = 6;
+            this.btnRenunta.Text = "Undo";
+            this.btnRenunta.UseVisualStyleBackColor = true;
+            this.btnRenunta.Click += new System.EventHandler(this.btnRenunta_Click);
+            // 
+            // EditForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(382, 444);
+            this.Controls.Add(this.btnRenunta);
+            this.Controls.Add(this.btnSalveaza);
+            this.Controls.Add(this.grBUserDetails);
+            this.Controls.Add(this.panelTop);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "EditForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "EditForm";
+            this.panelTop.ResumeLayout(false);
+            this.grBUserDetails.ResumeLayout(false);
+            this.grBUserDetails.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelTop;
+        private System.Windows.Forms.Button MaximizeBtn;
+        private System.Windows.Forms.Button CloseBtn;
+        private System.Windows.Forms.Button MinimizeBtn;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox grBUserDetails;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbUsername;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBoxVarsta;
+        private System.Windows.Forms.Label labelVars;
+        private System.Windows.Forms.TextBox tbEmail;
+        private System.Windows.Forms.Label labelEmail;
+        private System.Windows.Forms.TextBox tbNume;
+        private System.Windows.Forms.Label labelVarsta;
+        private System.Windows.Forms.TextBox tbPrenume;
+        private System.Windows.Forms.Label labelDenumire;
+        private System.Windows.Forms.Button btnSalveaza;
+        private System.Windows.Forms.Button btnRenunta;
+    }
+}
