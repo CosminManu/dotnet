a7bc764db611d8d645c053995adfb629478690f1
CosminManu <75031246+CosminManu@users.noreply.github.com>
2021-06-02 04:29:34 +0300

    implemented SQLite conn with injection

--- Fast Food demo/LogInForm.cs
+++ Fast Food demo/LogInForm.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SQLite;
 
 namespace Fast_Food_demo
 {
@@ -16,6 +17,9 @@ namespace Fast_Food_demo
     /// 
     /// Daca e timp implementat afisare formRegister
     /// in fata celui LogIn (min 23 https://www.youtube.com/watch?v=kcrOAF473LM&list=WL&index=53&t=6s)
+    /// video cu conex SQLite
+    /// https://www.youtube.com/watch?v=pde8dZEYIHA&t=460s
+    /// + inspirat cotfas
     /// </summary>
 
 
@@ -23,6 +27,10 @@ namespace Fast_Food_demo
     {
         bool drag = false;
         Point startPoint = new Point(0, 0);
+        private const string Connection = "Data Source=UserRegistrationDB.db";
+        private string username;
+        private string password;
+        private string confirmPass;
 
         public LogInForm()
         {
@@ -32,20 +40,52 @@ namespace Fast_Food_demo
         
         private void SignInBtn_Click(object sender, EventArgs e)
         {
-            // ...validari...
-
+            const string querySql = "SELECT Username, Password FROM User";
+            
             if (validPass() == true)
             {
-                this.Hide();
-                Form1 formularp = new Form1();
-                formularp.Show();
-                formularp.Closed += (s, args) => this.Close();      //exp lambda: creeaz func pe loc cand
-                                                                    //form p e inchis
-                //this.Close();                                     //s, args parametri (obj sender, EventArgs)
-            }
-            else
-            {
-                MessageBox.Show("Sign in failed.");
+                using (SQLiteConnection conn = new SQLiteConnection(Connection))
+                {
+                    conn.Open();
+                    var command = new SQLiteCommand(querySql, conn);    //init comanda cu queryul dat de mine
+
+                    using (SQLiteDataReader sqlReader = command.ExecuteReader())
+                    {
+                        while (sqlReader.Read())        //cat timp citeste urm linie
+                        {
+                            username = (string)sqlReader["Username"];
+                            password = (string)sqlReader["Password"];
+                            confirmPass = (string)sqlReader["Password"];
+                        }
+                    }  
+                }
+
+                if (usernameTb.Text == username)
+                {
+                    if (passwordTb.Text == password)
+                    {
+                        this.Hide();
+                        Form1 formularp = new Form1();
+                        formularp.Show();
+                        formularp.Closed += (s, args) => this.Close();      //exp lambda: creeaz func pe loc cand
+                                                                                //form p e inchis
+                                                                                //s, args parametri (obj sender, EventArgs)       
+                    }
+                    else
+                    {
+                        MessageBox.Show("Wrong password.",
+                                    "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        passwordTb.Clear();
+                        ConfirmPassTb.Clear();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Wrong username.", 
+                                    "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    usernameTb.Clear();
+                    //Application.Exit();
+                }
             }
             
         }
@@ -81,7 +121,7 @@ namespace Fast_Food_demo
             panel1.BackColor = Color.White;
             panel3.BackColor = Color.White;
 
-            validPass();
+            //validPass();
         }
 
         private void ConfirmPassTb_Click(object sender, EventArgs e)
@@ -96,17 +136,39 @@ namespace Fast_Food_demo
             panel1.BackColor = Color.White;
             panel2.BackColor = Color.White;
 
-            validPass();
+            //validPass();
           
         }
 
+
+        /// <summary>
+        /// functia verif daca a fost introd user
+        /// parola si confirm parola
+        /// </summary>
+        /// <returns>true / false</returns>
         private bool validPass()
         {
-            if (passwordTb.Text.Length != 0 && ConfirmPassTb.Text != passwordTb.Text)
+            if(usernameTb.Text.Length == 0)
+            {
+                string message = "Username is mandatory.";
+                string title = "Error";
+                MessageBox.Show(message, title, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return false;
+            }
+            else if (passwordTb.Text.Length == 0)
+            {
+                string message = "Password is mandatory.";
+                string title = "Error";
+                MessageBox.Show(message, title, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return false;
+            }
+            else if (ConfirmPassTb.Text != passwordTb.Text)
             {
                 string message = "Confirm password is not the same.";
                 string title = "Error";
                 MessageBox.Show(message, title, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                passwordTb.Clear();
+                ConfirmPassTb.Clear();
                 return false;
             }
             else return true;
