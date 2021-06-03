171130ada34376b8e5f8045553608f4b662ddac7
CosminManu <75031246+CosminManu@users.noreply.github.com>
2021-06-03 03:06:03 +0300

    final RegisterForm

--- Fast Food demo/RegisterForm.Designer.cs
+++ Fast Food demo/RegisterForm.Designer.cs
@@ -33,7 +33,7 @@ namespace Fast_Food_demo
             this.panel2 = new System.Windows.Forms.Panel();
             this.PrenumeTb = new System.Windows.Forms.MaskedTextBox();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.NameTb = new System.Windows.Forms.MaskedTextBox();
+            this.NumeTb = new System.Windows.Forms.MaskedTextBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panel3 = new System.Windows.Forms.Panel();
@@ -43,21 +43,28 @@ namespace Fast_Food_demo
             this.registerBtn = new System.Windows.Forms.Button();
             this.CloseBtn = new System.Windows.Forms.Button();
             this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.passwordTb = new System.Windows.Forms.MaskedTextBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.usernameTb = new System.Windows.Forms.MaskedTextBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
             this.SuspendLayout();
             // 
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.Color.White;
-            this.panel2.Location = new System.Drawing.Point(34, 201);
+            this.panel2.Location = new System.Drawing.Point(34, 174);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(240, 1);
             this.panel2.TabIndex = 15;
-            this.panel2.Click += new System.EventHandler(this.panel2_Click);
             // 
             // PrenumeTb
             // 
@@ -65,11 +72,11 @@ namespace Fast_Food_demo
             this.PrenumeTb.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.PrenumeTb.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.PrenumeTb.ForeColor = System.Drawing.Color.White;
-            this.PrenumeTb.Location = new System.Drawing.Point(74, 178);
+            this.PrenumeTb.Location = new System.Drawing.Point(74, 151);
             this.PrenumeTb.Name = "PrenumeTb";
             this.PrenumeTb.Size = new System.Drawing.Size(200, 15);
             this.PrenumeTb.TabIndex = 14;
-            this.PrenumeTb.Text = "Prenume";
+            this.PrenumeTb.Text = "First name";
             this.PrenumeTb.Click += new System.EventHandler(this.PrenumeTb_Click);
             this.PrenumeTb.Validating += new System.ComponentModel.CancelEventHandler(this.PrenumeTb_Validating);
             this.PrenumeTb.Validated += new System.EventHandler(this.PrenumeTb_Click);
@@ -77,31 +84,30 @@ namespace Fast_Food_demo
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.White;
-            this.panel1.Location = new System.Drawing.Point(34, 154);
+            this.panel1.Location = new System.Drawing.Point(34, 127);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(240, 1);
             this.panel1.TabIndex = 12;
-            this.panel1.Click += new System.EventHandler(this.panel1_Click);
             // 
-            // NameTb
+            // NumeTb
             // 
-            this.NameTb.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(39)))), ((int)(((byte)(40)))));
-            this.NameTb.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.NameTb.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.NameTb.ForeColor = System.Drawing.Color.White;
-            this.NameTb.Location = new System.Drawing.Point(74, 131);
-            this.NameTb.Name = "NameTb";
-            this.NameTb.Size = new System.Drawing.Size(200, 15);
-            this.NameTb.TabIndex = 11;
-            this.NameTb.Text = "Nume";
-            this.NameTb.Click += new System.EventHandler(this.NameTb_Click);
-            this.NameTb.Validating += new System.ComponentModel.CancelEventHandler(this.NameTb_Validating);
-            this.NameTb.Validated += new System.EventHandler(this.NameTb_Click);
+            this.NumeTb.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(39)))), ((int)(((byte)(40)))));
+            this.NumeTb.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.NumeTb.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NumeTb.ForeColor = System.Drawing.Color.White;
+            this.NumeTb.Location = new System.Drawing.Point(74, 104);
+            this.NumeTb.Name = "NumeTb";
+            this.NumeTb.Size = new System.Drawing.Size(200, 15);
+            this.NumeTb.TabIndex = 11;
+            this.NumeTb.Text = "Last name";
+            this.NumeTb.Click += new System.EventHandler(this.NumeTb_Click);
+            this.NumeTb.Validating += new System.ComponentModel.CancelEventHandler(this.NumeTb_Validating);
+            this.NumeTb.Validated += new System.EventHandler(this.NumeTb_Click);
             // 
             // pictureBox2
             // 
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(34, 123);
+            this.pictureBox2.Location = new System.Drawing.Point(34, 96);
             this.pictureBox2.Margin = new System.Windows.Forms.Padding(0);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(28, 28);
@@ -112,7 +118,7 @@ namespace Fast_Food_demo
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(34, 170);
+            this.pictureBox1.Location = new System.Drawing.Point(34, 143);
             this.pictureBox1.Margin = new System.Windows.Forms.Padding(0);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(28, 28);
@@ -123,11 +129,10 @@ namespace Fast_Food_demo
             // panel3
             // 
             this.panel3.BackColor = System.Drawing.Color.White;
-            this.panel3.Location = new System.Drawing.Point(34, 246);
+            this.panel3.Location = new System.Drawing.Point(34, 219);
             this.panel3.Name = "panel3";
             this.panel3.Size = new System.Drawing.Size(240, 1);
             this.panel3.TabIndex = 19;
-            this.panel3.Click += new System.EventHandler(this.panel3_Click);
             // 
             // EmailTb
             // 
@@ -135,11 +140,11 @@ namespace Fast_Food_demo
             this.EmailTb.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.EmailTb.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.EmailTb.ForeColor = System.Drawing.Color.White;
-            this.EmailTb.Location = new System.Drawing.Point(74, 223);
+            this.EmailTb.Location = new System.Drawing.Point(74, 196);
             this.EmailTb.Name = "EmailTb";
             this.EmailTb.Size = new System.Drawing.Size(200, 15);
             this.EmailTb.TabIndex = 18;
-            this.EmailTb.Text = "Email";
+            this.EmailTb.Text = "E-mail";
             this.EmailTb.Click += new System.EventHandler(this.EmailTb_Click);
             this.EmailTb.Validating += new System.ComponentModel.CancelEventHandler(this.EmailTb_Validating);
             this.EmailTb.Validated += new System.EventHandler(this.EmailTb_Click);
@@ -147,7 +152,7 @@ namespace Fast_Food_demo
             // pictureBox4
             // 
             this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
-            this.pictureBox4.Location = new System.Drawing.Point(34, 215);
+            this.pictureBox4.Location = new System.Drawing.Point(34, 188);
             this.pictureBox4.Margin = new System.Windows.Forms.Padding(0);
             this.pictureBox4.Name = "pictureBox4";
             this.pictureBox4.Size = new System.Drawing.Size(28, 28);
@@ -158,7 +163,7 @@ namespace Fast_Food_demo
             // pictureBox3
             // 
             this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
-            this.pictureBox3.Location = new System.Drawing.Point(115, 35);
+            this.pictureBox3.Location = new System.Drawing.Point(115, 21);
             this.pictureBox3.Margin = new System.Windows.Forms.Padding(0);
             this.pictureBox3.Name = "pictureBox3";
             this.pictureBox3.Size = new System.Drawing.Size(68, 51);
@@ -174,11 +179,12 @@ namespace Fast_Food_demo
             this.registerBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.registerBtn.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.registerBtn.ForeColor = System.Drawing.Color.White;
-            this.registerBtn.Location = new System.Drawing.Point(34, 318);
+            this.registerBtn.Location = new System.Drawing.Point(34, 329);
             this.registerBtn.Margin = new System.Windows.Forms.Padding(0);
             this.registerBtn.Name = "registerBtn";
             this.registerBtn.Size = new System.Drawing.Size(240, 32);
             this.registerBtn.TabIndex = 21;
+            this.registerBtn.TabStop = false;
             this.registerBtn.Text = "Register";
             this.registerBtn.UseVisualStyleBackColor = false;
             this.registerBtn.Click += new System.EventHandler(this.registerBtn_Click);
@@ -194,6 +200,7 @@ namespace Fast_Food_demo
             this.CloseBtn.Name = "CloseBtn";
             this.CloseBtn.Size = new System.Drawing.Size(24, 24);
             this.CloseBtn.TabIndex = 22;
+            this.CloseBtn.TabStop = false;
             this.CloseBtn.UseVisualStyleBackColor = false;
             this.CloseBtn.Click += new System.EventHandler(this.CloseBtn_Click);
             // 
@@ -201,12 +208,84 @@ namespace Fast_Food_demo
             // 
             this.errorProvider1.ContainerControl = this;
             // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.White;
+            this.panel5.Location = new System.Drawing.Point(33, 308);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(240, 1);
+            this.panel5.TabIndex = 28;
+            // 
+            // passwordTb
+            // 
+            this.passwordTb.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(39)))), ((int)(((byte)(40)))));
+            this.passwordTb.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.passwordTb.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passwordTb.ForeColor = System.Drawing.Color.White;
+            this.passwordTb.Location = new System.Drawing.Point(73, 285);
+            this.passwordTb.Name = "passwordTb";
+            this.passwordTb.Size = new System.Drawing.Size(200, 15);
+            this.passwordTb.TabIndex = 27;
+            this.passwordTb.Text = "Password";
+            this.passwordTb.Click += new System.EventHandler(this.passwordTb_Click);
+            this.passwordTb.Validating += new System.ComponentModel.CancelEventHandler(this.passwordTb_Validating);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
+            this.pictureBox5.Location = new System.Drawing.Point(33, 277);
+            this.pictureBox5.Margin = new System.Windows.Forms.Padding(0);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(28, 28);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 26;
+            this.pictureBox5.TabStop = false;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.White;
+            this.panel4.Location = new System.Drawing.Point(33, 263);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(240, 1);
+            this.panel4.TabIndex = 25;
+            // 
+            // usernameTb
+            // 
+            this.usernameTb.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(39)))), ((int)(((byte)(40)))));
+            this.usernameTb.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.usernameTb.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.usernameTb.ForeColor = System.Drawing.Color.White;
+            this.usernameTb.Location = new System.Drawing.Point(73, 240);
+            this.usernameTb.Name = "usernameTb";
+            this.usernameTb.Size = new System.Drawing.Size(200, 15);
+            this.usernameTb.TabIndex = 24;
+            this.usernameTb.Text = "Username";
+            this.usernameTb.Click += new System.EventHandler(this.usernameTb_Click);
+            this.usernameTb.Validating += new System.ComponentModel.CancelEventHandler(this.usernameTb_Validating);
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox6.Image")));
+            this.pictureBox6.Location = new System.Drawing.Point(33, 232);
+            this.pictureBox6.Margin = new System.Windows.Forms.Padding(0);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(28, 28);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 23;
+            this.pictureBox6.TabStop = false;
+            // 
             // RegisterForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(39)))), ((int)(((byte)(40)))));
             this.ClientSize = new System.Drawing.Size(318, 385);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.passwordTb);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.usernameTb);
+            this.Controls.Add(this.pictureBox6);
             this.Controls.Add(this.CloseBtn);
             this.Controls.Add(this.registerBtn);
             this.Controls.Add(this.pictureBox3);
@@ -217,7 +296,7 @@ namespace Fast_Food_demo
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.PrenumeTb);
             this.Controls.Add(this.panel1);
-            this.Controls.Add(this.NameTb);
+            this.Controls.Add(this.NumeTb);
             this.Controls.Add(this.pictureBox2);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "RegisterForm";
@@ -231,6 +310,8 @@ namespace Fast_Food_demo
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -240,8 +321,8 @@ namespace Fast_Food_demo
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.MaskedTextBox PrenumeTb;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.MaskedTextBox NameTb;
         private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.MaskedTextBox NumeTb;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Panel panel3;
         private System.Windows.Forms.MaskedTextBox EmailTb;
@@ -250,5 +331,11 @@ namespace Fast_Food_demo
         private System.Windows.Forms.Button registerBtn;
         private System.Windows.Forms.Button CloseBtn;
         private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.MaskedTextBox passwordTb;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.MaskedTextBox usernameTb;
+        private System.Windows.Forms.PictureBox pictureBox6;
     }
 }
