a5f4f1a397698c79e8b7f6cb3520404e5c15cba3
CosminManu <75031246+CosminManu@users.noreply.github.com>
2021-06-07 23:50:06 +0300

    made a editing form with cbbox

---
+++ Fast Food demo/EditFoodForm.Designer.cs
@@ -0,0 +1,254 @@
+﻿
+namespace Fast_Food_demo
+{
+    partial class EditFoodForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(EditFoodForm));
+            this.panelTop = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.MaximizeBtn = new System.Windows.Forms.Button();
+            this.CloseBtn = new System.Windows.Forms.Button();
+            this.MinimizeBtn = new System.Windows.Forms.Button();
+            this.ChooseLb = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SaveBtn = new System.Windows.Forms.Button();
+            this.panelLogo = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panelTop.SuspendLayout();
+            this.panelLogo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelTop
+            // 
+            this.panelTop.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(178)))), ((int)(((byte)(8)))), ((int)(((byte)(55)))));
+            this.panelTop.Controls.Add(this.button2);
+            this.panelTop.Controls.Add(this.button1);
+            this.panelTop.Controls.Add(this.MaximizeBtn);
+            this.panelTop.Controls.Add(this.CloseBtn);
+            this.panelTop.Controls.Add(this.MinimizeBtn);
+            this.panelTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTop.Location = new System.Drawing.Point(0, 0);
+            this.panelTop.Name = "panelTop";
+            this.panelTop.Size = new System.Drawing.Size(382, 20);
+            this.panelTop.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.Location = new System.Drawing.Point(300, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(21, 21);
+            this.button2.TabIndex = 3;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(337, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(21, 21);
+            this.button1.TabIndex = 18;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // MaximizeBtn
+            // 
+            this.MaximizeBtn.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.MaximizeBtn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("MaximizeBtn.BackgroundImage")));
+            this.MaximizeBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.MaximizeBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.MaximizeBtn.Location = new System.Drawing.Point(748, 1);
+            this.MaximizeBtn.Name = "MaximizeBtn";
+            this.MaximizeBtn.Size = new System.Drawing.Size(21, 20);
+            this.MaximizeBtn.TabIndex = 17;
+            this.MaximizeBtn.UseVisualStyleBackColor = false;
+            // 
+            // CloseBtn
+            // 
+            this.CloseBtn.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.CloseBtn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("CloseBtn.BackgroundImage")));
+            this.CloseBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.CloseBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.CloseBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CloseBtn.Location = new System.Drawing.Point(779, 1);
+            this.CloseBtn.Name = "CloseBtn";
+            this.CloseBtn.Size = new System.Drawing.Size(21, 21);
+            this.CloseBtn.TabIndex = 17;
+            this.CloseBtn.UseVisualStyleBackColor = false;
+            // 
+            // MinimizeBtn
+            // 
+            this.MinimizeBtn.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.MinimizeBtn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("MinimizeBtn.BackgroundImage")));
+            this.MinimizeBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.MinimizeBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.MinimizeBtn.FlatAppearance.BorderSize = 0;
+            this.MinimizeBtn.Location = new System.Drawing.Point(716, 1);
+            this.MinimizeBtn.Name = "MinimizeBtn";
+            this.MinimizeBtn.Size = new System.Drawing.Size(21, 21);
+            this.MinimizeBtn.TabIndex = 1;
+            this.MinimizeBtn.UseVisualStyleBackColor = false;
+            // 
+            // ChooseLb
+            // 
+            this.ChooseLb.AutoSize = true;
+            this.ChooseLb.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ChooseLb.Location = new System.Drawing.Point(24, 206);
+            this.ChooseLb.Name = "ChooseLb";
+            this.ChooseLb.Size = new System.Drawing.Size(107, 25);
+            this.ChooseLb.TabIndex = 4;
+            this.ChooseLb.Text = "Choose...";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "French Fries",
+            "Hamburger",
+            "Salad Box",
+            "Soda",
+            "Hot Wings",
+            "Choco Cake"});
+            this.comboBox1.Location = new System.Drawing.Point(29, 234);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(274, 21);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // SaveBtn
+            // 
+            this.SaveBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(178)))), ((int)(((byte)(8)))), ((int)(((byte)(55)))));
+            this.SaveBtn.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.SaveBtn.FlatAppearance.BorderSize = 0;
+            this.SaveBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.SaveBtn.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SaveBtn.ForeColor = System.Drawing.Color.White;
+            this.SaveBtn.Location = new System.Drawing.Point(29, 350);
+            this.SaveBtn.Name = "SaveBtn";
+            this.SaveBtn.Size = new System.Drawing.Size(93, 40);
+            this.SaveBtn.TabIndex = 19;
+            this.SaveBtn.Text = "Save";
+            this.SaveBtn.UseVisualStyleBackColor = false;
+            this.SaveBtn.Click += new System.EventHandler(this.SaveBtn_Click);
+            // 
+            // panelLogo
+            // 
+            this.panelLogo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(178)))), ((int)(((byte)(8)))), ((int)(((byte)(55)))));
+            this.panelLogo.Controls.Add(this.label3);
+            this.panelLogo.Controls.Add(this.label2);
+            this.panelLogo.Controls.Add(this.pictureBox1);
+            this.panelLogo.Location = new System.Drawing.Point(18, 20);
+            this.panelLogo.Name = "panelLogo";
+            this.panelLogo.Size = new System.Drawing.Size(129, 143);
+            this.panelLogo.TabIndex = 20;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(27, 89);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Restaurants";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(22, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 21);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Fast Food";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(8, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(103, 65);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // EditFoodForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(382, 444);
+            this.Controls.Add(this.panelLogo);
+            this.Controls.Add(this.SaveBtn);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.ChooseLb);
+            this.Controls.Add(this.panelTop);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "EditFoodForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "EditFoodForm";
+            this.panelTop.ResumeLayout(false);
+            this.panelLogo.ResumeLayout(false);
+            this.panelLogo.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelTop;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button MaximizeBtn;
+        private System.Windows.Forms.Button CloseBtn;
+        private System.Windows.Forms.Button MinimizeBtn;
+        private System.Windows.Forms.Label ChooseLb;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button SaveBtn;
+        private System.Windows.Forms.Panel panelLogo;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ Fast Food demo/EditFoodForm.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Fast_Food_demo.Modele;
+
+namespace Fast_Food_demo
+{
+    public partial class EditFoodForm : Form
+    {
+        Items buff;       // var locala (vizibila la celelalte comp clasa)
+        public EditFoodForm(Items s)
+        {
+            InitializeComponent();
+            buff = s;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Normal)
+                this.WindowState = FormWindowState.Minimized;
+            else if (this.WindowState == FormWindowState.Minimized)
+                this.WindowState = FormWindowState.Normal;
+        }
+
+        // Items <- EFood
+        //buff de tip Items => tip efood => contine incadrari cbbox
+
+        private void SaveBtn_Click(object sender, EventArgs e)
+        {
+            int n = comboBox1.SelectedIndex;
+            if(n == 0)
+            {
+                buff.efood = EFood.french_fries;
+                buff.Denumire = "French Fries";
+                buff.Cantitate++;
+                buff.Pret = 3.5;
+            }
+            else if(n == 1)
+            {
+                buff.efood = EFood.hamburger;
+                buff.Denumire = "Hamburger";
+                buff.Cantitate++;
+                buff.Pret = 5.5;
+            }
+            else if (n == 2)
+            {
+                buff.efood = EFood.salad_box;
+                buff.Denumire = "Salad Box";
+                buff.Cantitate++;
+                buff.Pret = 7;
+            }
+            else if (n == 3)
+            {
+                buff.efood = EFood.soda;
+                buff.Denumire = "Soda";
+                buff.Cantitate++;
+                buff.Pret = 2;
+            }
+            else if (n == 4)
+            {
+                buff.efood = EFood.hot_wings;
+                buff.Denumire = "Hot Wings";
+                buff.Cantitate++;
+                buff.Pret = 6.5;
+            }
+            else
+            {
+                buff.efood = EFood.choco_cake;
+                buff.Denumire = "Choco Cake";
+                buff.Cantitate++;
+                buff.Pret = 12;
+            }
+
+
+            MessageBox.Show("Data saved succesfully.", "Succes");
+            Close();
+        }
+    }
+}
