cba7d1b90ba34fb2475b4ccde30ff6aa0540bce2
CosminManu <75031246+CosminManu@users.noreply.github.com>
2021-06-07 23:53:35 +0300

    class w. id, first, last name, here/away order

---
+++ Fast Food demo/Modele/Order.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fast_Food_demo.Modele
+{
+    class Order
+    {
+        public long Id { get; set; }
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        public int here { get; set; }
+        public int away { get; set; }
+        public double total { get; set; }
+
+        public Order(string f, string l, int h, int a)
+        {
+            firstname = f;
+            lastname = l;
+            here = h;
+            away = a;
+        }
+
+        public Order( double tot)
+        {
+            total = tot;
+        }
+
+
+    }
+}
