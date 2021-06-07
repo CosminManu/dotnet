008755656ac764073b6d7c81ce9cc3f71d9ddc78
CosminManu <75031246+CosminManu@users.noreply.github.com>
2021-06-07 23:51:48 +0300

    created a new class which contains atrib for Food

---
+++ Fast Food demo/Modele/EFood.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fast_Food_demo.Modele
+{
+    public enum EFood
+    {
+        french_fries,          //index 0   
+        hamburger,          //index 1
+        salad_box,           //index 2
+        soda,                  //index 3
+        hot_wings,              //index 4
+        choco_cake              //index 5
+    
+    }
+}
